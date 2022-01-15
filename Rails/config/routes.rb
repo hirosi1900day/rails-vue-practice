dacd816647e99dd4f18fe1368159837e299f14da
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2022-01-15 13:52:54 +0900

    user一覧機能バックエンドがわ完成

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   namespace :api do
-    resources :users, only: %i[create]
+    resources :users, only: %i[create index]
     resource :session, only: %i[create destory]
     resources :microposts, only: %i[index show create update destroy]
     
